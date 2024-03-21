6d801b7cbbc054b0d0602a2da39cfe79aded785b
Atul <oraonatula@gmail.com>
2024-03-22 01:31:26 +0530

    Backend intiated

---
+++ backend/src/server.ts
@@ -0,0 +1,28 @@
+import exprsess from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+let app = exprsess();
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello Basic server running");
+});
+app.get("/api/users", (req, res) => {
+    res.send("Hello Users");
+});
+
+async function connectToMongo() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/test");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB");
+  }
+}
+
+connectToMongo();
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
