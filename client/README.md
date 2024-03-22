4e217c2030672ae597eaa7b418cd2f754d79e316
Atul <oraonatula@gmail.com>
2024-03-23 00:53:22 +0530

    This version of docker client works

--- client/README.md
+++ client/README.md
@@ -1,5 +1,11 @@
 # React + Vite
 
+# Current stable is 
+
+multi_dock_client                 0.014 
+
+with CMD CMD [ "npm", "run", "dev" ]
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port: 5173
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    port: 5000,
+    strictPort: true,
+    host: true,
+    origin: "http://localhost:5000",
   },
-  preview:{
+  preview: {
     host: "localhost",
-    port: 5000
-  }
-
+    port: 5000,
+    strictPort: true,
+  },
 });
