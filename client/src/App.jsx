d6c0c77e443ce43436d78cd810b2d2faf294a3c6
Atul <oraonatula@gmail.com>
2024-03-29 18:40:25 +0530

    Added .env to .dockerignore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  useEffect(() => {
+    console.log("HomePage Loaded")
+  },[])
 
   return (
   <>
