d92ac16694154f2f135219f39094e8bfacbbf6d4
Atul <oraonatula@gmail.com>
2024-03-29 19:28:49 +0530

    Added the link to the chat page in the home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,26 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   useEffect(() => {
-    console.log("HomePage Loaded")
-  },[])
+    console.log("HomePage Loaded");
+  }, []);
 
   return (
-  <>
-  <h1>Golu Page</h1>
-  </>
-  )
+    <>
+      <h1>Golu Page</h1>
+
+      <div className="flex justify-center items-center h-[30vh] mx-auto mt-[20vh] w-[20vw] bg-blue-500">
+        <Link to="/chat" className="font-bold text-white text-2xl block">
+          chatpage
+        </Link>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
